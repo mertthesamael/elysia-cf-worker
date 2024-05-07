56dedc39bf4dcf1522072916c2fcb0e70a25789b
Merto <mertenercan@gmail.com>
2024-05-07 06:08:05 +0300

    - removed try-catch block

--- src/routes/pokemon.ts
+++ src/routes/pokemon.ts
@@ -4,16 +4,17 @@ import { Elysia } from 'elysia'
 export const pokemonController = new Elysia({ prefix: "/pokemon" })
     .get("/:pokemonName", async (context) => {
         const { params: { pokemonName } } = context
-        try {
-            const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then((res) => res.json())
 
-            return new Response(JSON.stringify({ data: pokemonData }), { status: 200 })
-        } catch (err) {
-            throw Error(err instanceof Error ? err.message : 'Server Error')
+        const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        if (pokemonData.status !== 200) {
+            throw new Error(await pokemonData.text())
         }
+
+        return new Response(JSON.stringify({ data: await pokemonData.json() }), { status: 200 })
+
     })
