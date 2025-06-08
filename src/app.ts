9e18dc160a643842bc8d38d8a81622471a01ad8c
Merto <mertenercan@gmail.com>
2025-06-08 21:35:12 +0300

    - support for .env variables in deployment

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,5 @@ export const app = new Elysia({ aot: false }).onError(({ code, error }) => {
     console.log(code)
     return new Response(JSON.stringify({ error: error.toString() ?? code }), { status: 500 })
 }).use(pokemonController)
-
-
-app.get('/', () => "Hello from Elysia 🦊")
+.get('/', () => "Hello from Elysia 🦊")
+.get('/env', () => process.env.TEST_ENV)
